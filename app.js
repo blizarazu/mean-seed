fab3f8355e624c2f158c3f5c53b8e2edf355d99c
Beñat Lizarazu <blizarazu@users.noreply.github.com>
2015-05-15 20:41:05 +0200

    Refactor error handler

--- app.js
+++ app.js
@@ -16,36 +16,6 @@ require('./config/passport')();
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+require('./config/errorHandler')(app);
 
 module.exports = app;
